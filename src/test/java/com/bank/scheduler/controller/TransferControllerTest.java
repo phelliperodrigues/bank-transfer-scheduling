8fc7d3d4833ea9716e58bdd49befc13ac2170065
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2020-09-08 21:17:40 -0300

    Add profiles

--- src/test/java/com/bank/scheduler/controller/TransferControllerTest.java
+++ src/test/java/com/bank/scheduler/controller/TransferControllerTest.java
@@ -35,38 +35,38 @@ public class TransferControllerTest {
     @Test
     @DisplayName("[CREATE] - Should success scheduler a transfer")
     public void create() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
 
     @Test
     @DisplayName("[CREATE] - Should show exception when scheduler a invalid transfer")
     public void createInvalid() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should show message exceptions that value money required")
     public void createWithoutValue() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should show message exceptions that date transfer required")
     public void createWithoutDate() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should return rate for same day transfer")
     public void calculateRateSameDay() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should multiply the transfer rate up to 10 days, $12 a day")
     public void calculateRateUpTo10Day() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
 
@@ -74,37 +74,37 @@ public class TransferControllerTest {
     @Test
     @DisplayName("[CREATE] - Should returned Total rate if transfer is between 10 and 20 days, 8% rate")
     public void calculateRateBetween10And20Day() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should returned Total rate if transfer is between 20 and 30 days, 6% rate")
     public void calculateRateBetween20And30Day() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should returned Total rate if transfer is between 30 and 40 days, 4% rate")
     public void calculateRateBetween20And40Day() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should return rate for scheduler greater than 40 days and value greater $100.000, 2% rate")
     public void calculateRateGreater40DayAndGreater$100000() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[CREATE] - Should return rate for same day transfer")
     public void showExceptionNotFoundRate() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     @Test
     @DisplayName("[READ] - Should show all Transfer Scheduled")
     public void show() throws Exception {
-        Assertions.assertTrue(false);
+        Assertions.fail();
     }
 
     private Optional createValidTransfer() {
